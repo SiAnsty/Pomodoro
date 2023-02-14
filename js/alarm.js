560a1b135ee0cc4feae194a004b9349d2b23d0e8
AnastasyaSid <stasya-sidorova@mail.ru>
2023-02-14 14:59:21 +0600

    add js

--- js/alarm.js
+++ js/alarm.js
@@ -1,5 +1,12 @@
-const audio = new Audio('audio/san-andreas.mp3');
+import { state } from "./state.js";
+
+const audio = {
+work: new Audio('audio/san-andreas.mp3'),
+break: new Audio('audio/polnomochiya.mp3'),
+relax: new Audio('audio/strashno-ochen-strashno.mp3')
+}
+
 
 export const alarm = () => {
-  audio.play();
-}
+  audio[state.status].play();
+};
--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,18 @@
-import { state } from "./state.js"
+import { state } from "./state.js";
 import { alarm } from "./alarm.js";
+import { addZero } from "./util.js";
+import { changeActivBtn } from "./control.js";
 
 const minutesElem = document.querySelector('.time__minutes');
 const secondsElem = document.querySelector('.time__seconds');
-
-
-const showTime = (seconds) => {
-  minutesElem.textContent = Math.floor(seconds / 60);
-  secondsElem.textContent = seconds % 60;
+ 
+export const showTime = (seconds) => {
+  minutesElem.textContent = addZero(Math.floor(seconds / 60));
+  secondsElem.textContent = addZero(seconds % 60);
 }
 
 export const startTimer = () => {
-  state.timeLeft--;
+  state.timeLeft -= 1;
 
   showTime(state.timeLeft);
 
@@ -21,5 +22,20 @@ export const startTimer = () => {
 
   if (state.timeLeft <= 0) {
     alarm();
+
+    if (state.status === 'work') {
+      state.activeTodo.pomodoro += 1;
+
+      if (state.activeTodo.pomodoro % state.count) {
+        state.status = 'break'
+      } else {
+        state.status = 'relax'
+      }
+    } else {
+      state.status = 'work'
+    }
+    state.timeLeft = state[state.status] * 60;
+    changeActivBtn(state.status);
+    startTimer();
   }
 }
